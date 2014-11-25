1968538a71829f8206e2b230feacab8b1f4115f0
Peter Michaux <petermichaux@gmail.com>
2014-11-24 22:33:37 -0800

    increase copyright year

--- src/header.js
+++ src/header.js
@@ -1,6 +1,6 @@
 /*
 Grail version 1.0.4
-Copyright (c) 2013, Peter Michaux
+Copyright (c) 2014, Peter Michaux
 All rights reserved.
 Licensed under the Simplified BSD License.
 https://github.com/petermichaux/grail/blob/master/LICENSE
